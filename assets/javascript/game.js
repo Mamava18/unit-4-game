75b9cd517abe74decdb1176a53bd3c41fa332b59
Mamava18 <44853413+Mamava18@users.noreply.github.com>
2018-12-13 21:44:59 -0500

    Updating file

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var targetnumber = [19-120];
+
+$("#randNum").text(targetnumber);
+
+var counter;
+
+var valueGem = [1-12];
+
+for (var i = 0; i < valueGem.length; i++) {
+
+    var imageGem = $("<img>");
+
+    imageGem.attr("data-gemvalue", valueGem[i]);
+
+    
+}
+
